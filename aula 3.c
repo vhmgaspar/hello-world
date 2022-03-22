6ccc84ced9dca1fda6132cadc675e37206c0ead8
Vitor Hugo Mariano <vhmgaspar@hotmail.com>
2022-03-22 15:57:44 -0300

    first commit'

---
+++ aula 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {						// função obrigatória para o código rodar (int pois retorna 0 ou 1)
+	printf("Hello World!\n");		// impressão de algo na tela
+	return 0;
+}
+
+// funções usam letras minúsculas
+// especificação de variáveis
+
